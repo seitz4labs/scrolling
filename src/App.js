7bc1d30f002def8fe8fe68fbf41485a3dcbd34d7
Paulverizer <pseufzer@gmail.com>
2020-01-06 15:13:41 -0600

    Basic table working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
 import React from "react";
 import "./styles.css";
 
+function cols(n) {
+  let a = [];
+  for (let c = 0; c < n; c++) {
+    a.push({
+      width: "50px",
+      header: "Header " + c,
+      content: (row, i) => (
+        <span>
+          col {c} {row.a}
+        </span>
+      )
+    });
+  }
+  return a;
+}
+function rows(n) {
+  let a = [];
+  for (let i = 0; i < n; i++) {
+    a.push({ a: "row " + 1 });
+  }
+  return a;
+}
+
 export default function App() {
+  const famogo = cols(5);
+
+  const migs = rows(10);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <div id="Migrations" className="caramel-section">
+        <div
+          className="caramel-scrollablex"
+          style={{ minWidth: "100vw", maxWidth: "100vw" }}
+        >
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            {famogo.map(col => (
+              <div
+                style={{
+                  display: "flex",
+                  minWidth: col.width,
+                  maxWidth: col.width,
+                  backgroundColor: "#fee"
+                }}
+              >
+                {col.header}
+              </div>
+            ))}
+          </div>
+          <div className="caramel-scrollabley">
+            {migs.map((row, i) => (
+              <div
+                key={row.runId}
+                style={{ display: "flex", flexDirection: "row" }}
+              >
+                {famogo.map(col => (
+                  <div
+                    style={{
+                      display: "flex",
+                      minWidth: col.width,
+                      maxWidth: col.width,
+                      backgroundColor: "#eef"
+                    }}
+                  >
+                    {col.content(row, i)}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
